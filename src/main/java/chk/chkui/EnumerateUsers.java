33834dfc89fbd52d85f3e959e9fff9d123874b9e
chk <chikinrasshu@yahoo.co.jp>
2022-04-02 16:33:06 -0300

    EnumerateUsers llenar JTree con las reparticiones
    Al crear un EnumerateUsers con un gobierno, este llena el JTree con los valores de getReparticiones(), pero aun no se hace nada con ellos.

--- src/main/java/chk/chkui/EnumerateUsers.java
+++ src/main/java/chk/chkui/EnumerateUsers.java
@@ -5,6 +5,10 @@
  */
 package chk.chkui;
 
+import gobierno.Reparticion;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
 /**
  *
  * @author chkp
@@ -13,9 +17,11 @@ public class EnumerateUsers extends javax.swing.JFrame {
 
     /**
      * Creates new form EnumerateUsers
+     * @param gobierno
      */
-    public EnumerateUsers() {
+    public EnumerateUsers(gobierno.Gobierno gobierno) {
         initComponents();
+        setGobierno(gobierno);
     }
 
     /**
@@ -26,86 +32,86 @@ public class EnumerateUsers extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
-        jSplitPane1 = new javax.swing.JSplitPane();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTree1 = new javax.swing.JTree();
-        jPanel4 = new javax.swing.JPanel();
+        gobierno = new gobierno.Gobierno();
+        splitPanel = new javax.swing.JSplitPane();
+        leftPanel = new javax.swing.JPanel();
+        reparticionTitle = new javax.swing.JLabel();
+        reparticionScrollPane = new javax.swing.JScrollPane();
+        reparticionTree = new javax.swing.JTree();
+        leftButtonsPanel = new javax.swing.JPanel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
+        rightPanel = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jPanel6 = new javax.swing.JPanel();
+        trabajadoresScrollPane = new javax.swing.JScrollPane();
+        trabajadoresTable = new javax.swing.JTable();
+        rightButtonsPanel = new javax.swing.JPanel();
         jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
         jButton9 = new javax.swing.JButton();
         jButton10 = new javax.swing.JButton();
         jButton11 = new javax.swing.JButton();
         jButton12 = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
+        mainButtonsPanel = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jSplitPane1.setDividerLocation(256);
+        splitPanel.setDividerLocation(256);
 
-        jLabel1.setText("Departments");
+        reparticionTitle.setText("Reparticiones");
 
-        jScrollPane1.setViewportView(jTree1);
+        reparticionScrollPane.setViewportView(reparticionTree);
 
-        jPanel4.setBorder(javax.swing.BorderFactory.createEmptyBorder(4, 4, 4, 4));
-        jPanel4.setLayout(new java.awt.GridLayout(2, 2, 4, 4));
+        leftButtonsPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(4, 4, 4, 4));
+        leftButtonsPanel.setLayout(new java.awt.GridLayout(2, 2, 4, 4));
 
         jButton3.setText("jButton3");
-        jPanel4.add(jButton3);
+        leftButtonsPanel.add(jButton3);
 
         jButton4.setText("jButton4");
-        jPanel4.add(jButton4);
+        leftButtonsPanel.add(jButton4);
 
         jButton5.setText("jButton5");
-        jPanel4.add(jButton5);
+        leftButtonsPanel.add(jButton5);
 
         jButton6.setText("jButton6");
-        jPanel4.add(jButton6);
+        leftButtonsPanel.add(jButton6);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout leftPanelLayout = new javax.swing.GroupLayout(leftPanel);
+        leftPanel.setLayout(leftPanelLayout);
+        leftPanelLayout.setHorizontalGroup(
+            leftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(leftPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                .addGroup(leftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(reparticionScrollPane)
+                    .addGroup(leftPanelLayout.createSequentialGroup()
+                        .addComponent(reparticionTitle)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(leftButtonsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        leftPanelLayout.setVerticalGroup(
+            leftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(leftPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(reparticionTitle)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1)
+                .addComponent(reparticionScrollPane)
                 .addGap(18, 18, 18)
-                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(leftButtonsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jSplitPane1.setLeftComponent(jPanel2);
+        splitPanel.setLeftComponent(leftPanel);
 
-        jLabel2.setText("Users");
+        jLabel2.setText("Trabajadores");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        trabajadoresTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -116,76 +122,76 @@ public class EnumerateUsers extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane2.setViewportView(jTable1);
+        trabajadoresScrollPane.setViewportView(trabajadoresTable);
 
-        jPanel6.setBorder(javax.swing.BorderFactory.createEmptyBorder(4, 4, 4, 4));
-        jPanel6.setLayout(new java.awt.GridLayout(2, 3, 4, 4));
+        rightButtonsPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(4, 4, 4, 4));
+        rightButtonsPanel.setLayout(new java.awt.GridLayout(2, 3, 4, 4));
 
         jButton7.setText("jButton7");
-        jPanel6.add(jButton7);
+        rightButtonsPanel.add(jButton7);
 
         jButton8.setText("jButton8");
-        jPanel6.add(jButton8);
+        rightButtonsPanel.add(jButton8);
 
         jButton9.setText("jButton9");
-        jPanel6.add(jButton9);
+        rightButtonsPanel.add(jButton9);
 
         jButton10.setText("jButton10");
-        jPanel6.add(jButton10);
+        rightButtonsPanel.add(jButton10);
 
         jButton11.setText("jButton11");
-        jPanel6.add(jButton11);
+        rightButtonsPanel.add(jButton11);
 
         jButton12.setText("jButton12");
-        jPanel6.add(jButton12);
+        rightButtonsPanel.add(jButton12);
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        javax.swing.GroupLayout rightPanelLayout = new javax.swing.GroupLayout(rightPanel);
+        rightPanel.setLayout(rightPanelLayout);
+        rightPanelLayout.setHorizontalGroup(
+            rightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(rightPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(rightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(trabajadoresScrollPane)
+                    .addGroup(rightPanelLayout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(rightButtonsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        rightPanelLayout.setVerticalGroup(
+            rightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(rightPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE)
+                .addComponent(trabajadoresScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(rightButtonsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jSplitPane1.setRightComponent(jPanel3);
+        splitPanel.setRightComponent(rightPanel);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Volver atras");
 
-        jButton2.setText("jButton2");
+        jButton2.setText("Ayuda");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout mainButtonsPanelLayout = new javax.swing.GroupLayout(mainButtonsPanel);
+        mainButtonsPanel.setLayout(mainButtonsPanelLayout);
+        mainButtonsPanelLayout.setHorizontalGroup(
+            mainButtonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainButtonsPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addContainerGap())
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        mainButtonsPanelLayout.setVerticalGroup(
+            mainButtonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainButtonsPanelLayout.createSequentialGroup()
                 .addGap(0, 10, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(mainButtonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
                 .addGap(10, 10, 10))
@@ -195,21 +201,40 @@ public class EnumerateUsers extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jSplitPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 748, Short.MAX_VALUE)
+            .addComponent(mainButtonsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(splitPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 748, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jSplitPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE)
+                .addComponent(splitPanel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(mainButtonsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    public void setGobierno(gobierno.Gobierno gobierno) {
+        this.gobierno = gobierno;
+        reloadGobierno();
+    }
+    
+    public void reloadGobierno() {
+        DefaultMutableTreeNode treeRoot = new DefaultMutableTreeNode();
+        // populate the nodes
+        DefaultMutableTreeNode reparticionNode = null;
+        for(Reparticion reparticion : gobierno.getReparticiones()){
+            reparticionNode = new DefaultMutableTreeNode(reparticion.getNombre());
+            treeRoot.add(reparticionNode);
+        }
+        
+        DefaultTreeModel treeModel = new DefaultTreeModel(treeRoot);
+        this.reparticionTree.setModel(treeModel);
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private gobierno.Gobierno gobierno;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
@@ -222,18 +247,17 @@ public class EnumerateUsers extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JSplitPane jSplitPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTree jTree1;
+    private javax.swing.JPanel leftButtonsPanel;
+    private javax.swing.JPanel leftPanel;
+    private javax.swing.JPanel mainButtonsPanel;
+    private javax.swing.JScrollPane reparticionScrollPane;
+    private javax.swing.JLabel reparticionTitle;
+    private javax.swing.JTree reparticionTree;
+    private javax.swing.JPanel rightButtonsPanel;
+    private javax.swing.JPanel rightPanel;
+    private javax.swing.JSplitPane splitPanel;
+    private javax.swing.JScrollPane trabajadoresScrollPane;
+    private javax.swing.JTable trabajadoresTable;
     // End of variables declaration//GEN-END:variables
 }
